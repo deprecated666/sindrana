bc8a9517c89a98c32fd87d86aebdd1153ac4fe73
deprecated666 <adiska.chandra0101@student.untan.ac.id>
2022-04-06 16:31:12 +0700

    OK 1

---
+++ application/views/front/v_beranda.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/main.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>icons/css/all.min.css">
+    <title>SINDRANA</title>
+</head>
+
+<body>
+
+    <div class="title">
+        <div class="row">
+            <div class="col-12">
+                <h1>SINDRANA</h1>
+            </div>
+            <div class="col-12">
+                <p>Sistem Informasi Daerah Rawan Bencana</p>
+            </div>
+        </div>
+    </div>
+    <div class="logo">
+        <div class="row">
+            <div class="col-12">
+                <img src="<?= base_url('assets/') ?>img/logosanggau.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="tombol">
+        <a href="<?= base_url('Infopeta') ?>" class="btn1">
+            <i class="fa fa-map" aria-hidden="true"> </i>
+            Info Peta
+        </a>
+        <a href="#" class="btn2">
+            <i class="fas fa-sign-in-alt" aria-hidden="true"> </i>
+            Login
+        </a>
+    </div>
+
+
+
+
+</body>
+
+<script href="<?= base_url('assets/') ?>icons/js/all.min.js"></script>
+
+</html>
---
+++ application/views/front/v_info_peta.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/main.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>icons/css/all.min.css">
+    <title>Info Peta</title>
+</head>
+
+<body>
+
+</body>
+
+<script href="<?= base_url('assets/') ?>icons/js/all.min.js"></script>
+
+</html>
